c2220688e0a51bcbc86397982ce80aa55059487a
MichalT <trzebuniak.michal@gmail.com>
2023-02-13 16:03:51 +0100

    Rework

--- Analyze.py
+++ Analyze.py
@@ -1,56 +1,45 @@
-def runAnalysis(fileNames):
+def generateDiffFiles(fileName, prefixes):
 
     import json
     userAgentDict = {}
 
-    raportFile = open("raport", "w")
+    raportFile = open("raport", "a")
+    raportFile.write("Starting a new download for" + fileName)
 
     class Record:
         def __init__(self, timestamp, elapsed_ms):
             self.timestamp = timestamp
             self.elapsed_ms = elapsed_ms
 
-    # def sortUserAgentBucket(arr):
-    #     span = int(len(arr) / 1.3)
-    #     while span >= 1:
-    #         i = 0
-    #         while i + span < len(arr):
-    #             if arr[i].timestamp > arr[i + span].timestamp:
-    #                 arr[i], arr[i + span] = arr[i + span], arr[i]
-    #             i += 1
-    #         span = int(span / 1.3)
-
-    for name in fileNames:
-        f = open(name, 'r')
+    # Load files into memory
+    for prefix in prefixes:
+        f = open(prefix+fileName, 'r')
         for i, line in enumerate(f):
             if i % 10e6 == 0:
                 raportFile.write(str(i) + "\n")
                 print(i)
 
             jsonLine = json.loads(line)
-            if True or 'dn' in jsonLine: # HACK
-                if 'userAgent' in jsonLine and 'timestamp' in jsonLine and 'elapsed_ms' in jsonLine:
-                    userAgent = jsonLine['userAgent']
-                    if userAgent not in userAgentDict:
-                        userAgentDict[userAgent] = []
-                    userAgentDict[userAgent].append(Record(jsonLine['timestamp'], jsonLine['elapsed_ms']))
+            if 'userAgent' in jsonLine and 'timestamp' in jsonLine and 'elapsed_ms' in jsonLine:
+                userAgent = jsonLine['userAgent']
+                if userAgent not in userAgentDict:
+                    userAgentDict[userAgent] = []
+                userAgentDict[userAgent].append(Record(jsonLine['timestamp'], jsonLine['elapsed_ms']))
         f.close()
 
     raportFile.write("Reading complete\n")
     print("Reading complete")
 
     for i in userAgentDict:
-        # sortUserAgentBucket(userAgentDict[i])
-        userAgentDict[i].sort(key = lambda x: x.timestamp)
+        userAgentDict[i].sort(key=lambda x: x.timestamp)
 
     raportFile.write("User agent containers sorted\n")
     print("User agent containers sorted")
 
-    zero_diff = 0
-    one_diff = 0
-    large_diff = 0
-    allDiffs = 0
+    d = open("diffs_" + fileName, "w")
+
     for i in userAgentDict:
+        d.write("!" + i + "\n")
         for j in range(1, len(userAgentDict[i])):
             if userAgentDict[i][j].timestamp < userAgentDict[i][j - 1].timestamp:
                 raportFile.write("ERROR IN SORTING\n")
@@ -58,37 +47,11 @@ def runAnalysis(fileNames):
                 exit()
 
             diff = int(userAgentDict[i][j].timestamp) - int(float(userAgentDict[i][j - 1].elapsed_ms)) - int(userAgentDict[i][j - 1].timestamp)
-            allDiffs += 1
+            d.write(str(diff) + "\n")
 
-            if diff == 0:
-                zero_diff += 1
-            elif diff == -1:
-                one_diff += 1
-            elif diff < -1:
-                large_diff += 1
-
-    print(allDiffs)
-    print(zero_diff, one_diff, large_diff)
-    print(zero_diff + one_diff + large_diff)
-    raportFile.write("All diffs (including positive " + str(allDiffs))
-    raportFile.write("Zero, one and large diff: " + str(zero_diff) + " " + str(one_diff) + " " + str(large_diff) + "\n")
-    raportFile.write("Total diffs " + str(zero_diff + one_diff + large_diff))
+    d.close()
 
     print("Check complete")
     raportFile.write("Check complete\n")
     raportFile.close()
 
-    f = open("diffs_" + "_".join(fileNames), "w")
-    for i in userAgentDict:
-        for j in range(1, len(userAgentDict[i])):
-            diff = int(userAgentDict[i][j].timestamp) - int(float(userAgentDict[i][j - 1].elapsed_ms)) - int(userAgentDict[i][j - 1].timestamp)
-            f.write(str(diff) + "\n")
-    f.close()
-
-    # out = open('out.txt', 'w')
-    # for i in userAgentDict:
-    #     for j in range(len(userAgentDict[i])):
-    #         out.write(str(i) + "!" + str(userAgentDict[i][j].timestamp) + "!" + str(userAgentDict[i][j].elapsed_ms) + "\n")
-    # out.close()
-    #
-    # print("Results saved")
--- Multitasker.py
+++ Multitasker.py
@@ -1,22 +1,30 @@
-from Analyze import runAnalysis
 import os
 
-def download(serverName, fileName):
+from Analyze import generateDiffFiles
 
 
-    os.system("wget " + serverName + fileName + ".bz2")
-    os.system("mv " + fileName + " alicdb1/" + fileName + ".bz2")
+def download(serverName, fileName, prefixes):
 
-    # os.system("wget -o alicdb1/"+fileName+".bz2 " + serverName + "alicdb1/" + str(fileName) + ".bz2")
-    # os.system("rm " + fileName + ".bz2")
-    # # os.system("bzip2 -d alicdb1/" + fileName + ".bz2")
-    # os.system("wget -o alicdb2/"+fileName+".bz2 " + serverName + "alicdb2/" + str(fileName) + ".bz2")
-    # os.system("rm " + fileName + ".bz2")
-    # # os.system("bzip2 -d alicdb1/2" + fileName + ".bz2")
+    for prefix in prefixes:
+        if not os.path.isfile(prefix+fileName):  # If file is not present
+            if not os.path.isfile(prefix + fileName + ".bz2"):  # If archive is not present
+                print("Downloading", prefix+fileName)
+                os.system("wget " + serverName + prefix + fileName + ".bz2")  # Download archive
+                os.system("mv " + fileName + ".bz2 " + prefix+fileName + ".bz2")  # Move the archive to the destination
+            print("Unpacking", prefix+fileName)
+            os.system("bzip2 -cd " + prefix+fileName+".bz2 > "+prefix+fileName)  # Unpack the archive in destination
 
+def remove(fileName, prefixes):
+    for prefix in prefixes:
+        os.system("rm " + prefix + fileName)
+
+
+prefixes = ["alicdb1/", "alicdb2/"]
 serverName = "http://alimonitor.cern.ch/download/michal/"
 fileNames = ['http_access_log.json-20221120']
-for name in fileNames:
-    download(serverName, name)
 
-# runAnalysis(fileNames)
+for name in fileNames:
+    if not os.path.isfile("./diffs_"+name):
+        download(serverName, name, prefixes)
+        generateDiffFiles(name, prefixes)
+        remove(name, prefixes)
