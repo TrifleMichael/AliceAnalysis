4617982047a8dc1e20a2f6a83c9b89aeecde976b
MichalT <trzebuniak.michal@gmail.com>
2023-03-15 16:30:14 +0100

    New task

--- Multitasker.py
+++ Multitasker.py
@@ -1,4 +1,5 @@
 import os
+import traceback
 
 from Analyze import generateDiffFiles
 from open_window_analysis import generateConnectionsGraph
@@ -27,51 +28,60 @@ def remove(fileName, prefixes):
 
 prefixes = ["alicdb1/", "alicdb2/"]
 serverName = "http://alimonitor.cern.ch/download/michal/"
-# fileNames = ['http_access_log.json-20221102']
 
 fileNames = [
-    # "http_access_log.json-20221116",
-    # "http_access_log.json-20221121",
-    # "http_access_log.json-20221112",
-    # "http_access_log.json-20221101",
-    # "http_access_log.json-20221113",
-    "http_access_log.json-20221118",
-    "http_access_log.json-20221103",
-    "http_access_log.json-20221117",
-    "http_access_log.json-20221107",
-    "http_access_log.json-20221111",
-    "http_access_log.json-20221123",
-    "http_access_log.json-20221119",
-    "http_access_log.json-20221110",
-    "http_access_log.json-20221109",
-    "http_access_log.json-20221105",
-    "http_access_log.json-20221104",
-    "http_access_log.json-20221126",
-    "http_access_log.json-20221128",
-    "http_access_log.json-20221106",
-    "http_access_log.json-20221114",
-    "http_access_log.json-20221127",
-    "http_access_log.json-20221115",
-    "http_access_log.json-20221108",
-    "http_access_log.json-20221124",
-    "http_access_log.json-20221122",
-    "http_access_log.json-20221125",
-    "http_access_log.json-20221102",
-    "http_access_log.json-20221120"
+    "http_access_log.json-20230312",
+    "http_access_log.json-20230313",
+    "http_access_log.json-20230314"
 ]
 
-# for name in fileNames:
-#     if not os.path.isfile("diff_files/diffs_"+name):
-#         download(serverName, name, prefixes)
-#         generateDiffFiles(name, prefixes)
-#         remove(name, prefixes)
+# fileNames = [
+# "http_access_log.json-20221116",
+# "http_access_log.json-20221121",
+# "http_access_log.json-20221112",
+# "http_access_log.json-20221101",
+# "http_access_log.json-20221113",
+# "http_access_log.json-20221118",
+# "http_access_log.json-20221103",
+# "http_access_log.json-20221117",
+# "http_access_log.json-20221107",
+# "http_access_log.json-20221111",
+# "http_access_log.json-20221123",
+# "http_access_log.json-20221119",
+# "http_access_log.json-20221110",
+# "http_access_log.json-20221109",
+# "http_access_log.json-20221105",
+# "http_access_log.json-20221104",
+# "http_access_log.json-20221126",
+# "http_access_log.json-20221128",
+# "http_access_log.json-20221106",
+# "http_access_log.json-20221114",
+# "http_access_log.json-20221127",
+# "http_access_log.json-20221115",
+# "http_access_log.json-20221108",
+# "http_access_log.json-20221124",
+# "http_access_log.json-20221122",
+# "http_access_log.json-20221125",
+# "http_access_log.json-20221102",
+# "http_access_log.json-20221120"
+# ]
+
+# CONNECTION TIME ANALYSIS
+
+def connection_time_analysis(fileNames):
+    for name in fileNames:
+        if not os.path.isfile("diff_files/diffs_" + name):
+            download(serverName, name, prefixes)
+            generateDiffFiles(name, prefixes)
+            remove(name, prefixes)
+
 
 try:
     window_sizes = [100]
     for name in fileNames:
         for size in window_sizes:
             outputName = str(size) + "_" + name + ".png"
-            if not os.path.isfile("./output/"+outputName+"_results"):
+            if not os.path.isfile("./output/" + outputName + "_results"):
                 log("Starting download for: " + name)
                 download(serverName, name, prefixes)
                 log("Preparing graph for: " + name)
@@ -85,3 +95,4 @@ except Exception as ex:
     template = "An exception of type {0} occurred. Arguments:\n{1!r}"
     message = template.format(type(ex).__name__, ex.args)
     log(message)
+    print(traceback.format_exc())
