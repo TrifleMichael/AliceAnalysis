ddf20139e406519e86b73e5c48f143c557b5a106
MichalT <trzebuniak.michal@gmail.com>
2023-03-15 16:35:32 +0100

    Better checks for finished tasks

--- Multitasker.py
+++ Multitasker.py
@@ -9,15 +9,19 @@ from open_window_analysis import log
 
 def download(serverName, fileName, prefixes):  # TODO: Log errors
     for prefix in prefixes:
-        if not os.path.isfile(prefix + fileName):  # If file is not present
-            if not os.path.isfile(prefix + fileName + ".bz2"):  # If archive is not present
-                print("Downloading", prefix + fileName)
+        if not os.path.isfile(prefix + fileName + ".bz2"):  # If archive is not present
+            if not os.path.isfile(prefix + fileName):  # If file is not present
+                log("Downloading" + prefix + fileName)
                 os.system("wget " + serverName + prefix + fileName + ".bz2")  # Download archive
                 os.system(
                     "mv " + fileName + ".bz2 " + prefix + fileName + ".bz2")  # Move the archive to the destination
+            else:
+                log("Skipping download for "+fileName)
             print("Unpacking", prefix + fileName)
             os.system(
                 "bzip2 -cd " + prefix + fileName + ".bz2 > " + prefix + fileName)  # Unpack the archive in destination
+        else:
+            log("Skipping unpacking for ")
 
 
 def remove(fileName, prefixes):
@@ -35,38 +39,7 @@ fileNames = [
     "http_access_log.json-20230314"
 ]
 
-# fileNames = [
-# "http_access_log.json-20221116",
-# "http_access_log.json-20221121",
-# "http_access_log.json-20221112",
-# "http_access_log.json-20221101",
-# "http_access_log.json-20221113",
-# "http_access_log.json-20221118",
-# "http_access_log.json-20221103",
-# "http_access_log.json-20221117",
-# "http_access_log.json-20221107",
-# "http_access_log.json-20221111",
-# "http_access_log.json-20221123",
-# "http_access_log.json-20221119",
-# "http_access_log.json-20221110",
-# "http_access_log.json-20221109",
-# "http_access_log.json-20221105",
-# "http_access_log.json-20221104",
-# "http_access_log.json-20221126",
-# "http_access_log.json-20221128",
-# "http_access_log.json-20221106",
-# "http_access_log.json-20221114",
-# "http_access_log.json-20221127",
-# "http_access_log.json-20221115",
-# "http_access_log.json-20221108",
-# "http_access_log.json-20221124",
-# "http_access_log.json-20221122",
-# "http_access_log.json-20221125",
-# "http_access_log.json-20221102",
-# "http_access_log.json-20221120"
-# ]
 
-# CONNECTION TIME ANALYSIS
 
 def connection_time_analysis(fileNames):
     for name in fileNames:
@@ -82,7 +55,7 @@ try:
         for size in window_sizes:
             outputName = str(size) + "_" + name + ".png"
             if not os.path.isfile("./output/" + outputName + "_results"):
-                log("Starting download for: " + name)
+                log("Considering download for: " + name)
                 download(serverName, name, prefixes)
                 log("Preparing graph for: " + name)
                 generateConnectionsGraph([prefixes[0] + name, prefixes[1] + name], outputName, size)
