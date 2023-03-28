2539220f67c96619ba4a8a516188870fc2415b5c
MichalT <trzebuniak.michal@gmail.com>
2023-03-28 18:23:24 +0200

    New task

--- concurrent_plot.py
+++ concurrent_plot.py
@@ -1,8 +1,10 @@
 import matplotlib.pyplot as plt
 import os
 
-input_path = "./keep_alive_estimates/"
-output_path = "./concurrent_connections_plots/"
+# input_path = "./keep_alive_estimates/"
+# output_path = "./concurrent_connections_plots/"
+input_path = "./connection_starts/"
+output_path = "./connection_starts_plots/"
 hours_per_ticks = 2
 
 filenames = os.listdir(input_path)
